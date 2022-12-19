7f2475fadeae9cbc474b0e45d978c3769ee91a36
Jean-François Bilodeau <jfbilodeau@chronogears.com>
2022-12-19 08:07:56 -0500

    Added 'format' instead of 'raw' to generate kc document

--- routes/kc.js
+++ routes/kc.js
@@ -1,29 +1,34 @@
 import express from 'express'
 import * as scrapper from '../scrapper.js'
 
-const router = express.Router();
+const router = express.Router()
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-    res.render('kc/index')
+  res.render('kc/index')
 })
 
 router.get(`/document`, async (req, res, next) => {
-    const courseId = req.query.courseId?.toLowerCase()
-    const locale = req.query.locale?.toLowerCase() ?? `en-us`
-    const raw = req.query.raw
+  const courseId = req.query.courseId?.toLowerCase()
+  const locale = req.query.locale?.toLowerCase() ?? `en-us`
+  const format = req.query.format?.toLowerCase() ?? (req.query.raw ? 'json' : null)
 
-    if (courseId) {
-        const studyGuide = await scrapper.getStudyGuide(courseId, locale)
+  if (courseId) {
+    const studyGuide = await scrapper.getStudyGuide(courseId, locale)
 
-        if (raw) {
-            res.json(studyGuide)
-        } else {
-            res.render(`kc/document`, studyGuide)
-        }
-    } else {
-        res.redirect(`index`)
+    switch (format) {
+      case 'json':
+        res.json(studyGuide)
+        break
+      case 'html':
+        res.render(`kc/document`, studyGuide)
+        break
+      default:
+        res.status(400).send(`format must be 'json' or 'html'`)
     }
+  } else {
+    res.redirect(`index`)
+  }
 })
 
-export default router;
+export default router
