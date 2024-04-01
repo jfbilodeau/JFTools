d5d91c41711be3228b24897355aafb3fbaf8c18b
Jean-François Bilodeau <jfbilodeau@chronogears.com>
2024-04-01 10:46:10 -0400

    GenScript works.

--- routes/delivery.js
+++ routes/delivery.js
@@ -2,7 +2,7 @@ import express from 'express'
 import * as auth from '../auth.js'
 import * as cosmos from '../cosmos.js'
 import * as QRCode from 'qrcode'
-// import * as openai from ''
+import * as openai from '@azure/openai'
 
 const router = express.Router();
 
@@ -34,14 +34,64 @@ router.get('/genquiz', auth.authenticated, async function (req, res, next) {
     return
   }
 
-  const description = req.query.description
-  var script = ''
+  let description = req.query.description
+  let script = ''
 
-  if (description) {
+  if (!description) {
+    description = `Generate a three question quiz on security for Azure DevOps pipeline. Use the title 'Day 2 - Review'`
+  } else {
+    const client = new openai.OpenAIClient(
+      settings.openAiUrl,
+      new openai.AzureKeyCredential(settings.openAiKey),
+    )
 
+    const systemMessage = `
+      You are 'GenQuiz', an AI that generates quiz questions based on a given text.
+      The quiz will be multiple choice, with 4 possible answers. If the number of questions to generate is not specified, generate 5 questions.
+      The first line of the answer will be the raw title of the quiz, followed by a blank line.
+        - Do not add a label such as 'title:' before the quiz title.
+      
+      For each question:
+        - Provide the question on a single line, followed by the four possible answers. 
+        - Start the question with the label 'Poll: '
+        - Prefix the answers with a dash and a space ('- '). 
+        - The last line of the question will start with a pound sign ('#') and include the right answer with a short, one sentence, explanation of the answer.
+    `
+
+    const conversation = [
+      {
+        role: 'system',
+        content: systemMessage,
+      },
+      {
+        role: 'user',
+        content: description,
+      },
+    ]
+
+    const options = {
+      max_tokens: 1500,
+      n: 1,
+
+      temperature: 1.0,
+      top_p: 1.0,
+    }
+
+    try {
+      const response = await client.getChatCompletions(
+        settings.openAiDeployment,
+        conversation,
+        options,
+      )
+
+      script = response.choices[0].message.content
+    } catch (e) {
+      console.error(e)
+      script = `Error generating quiz. Reason: ${e.message}`
+    }
   }
 
-  res.render('delivery/genquiz', { title: 'Generate Quiz', script })
+  res.render('delivery/genquiz', { title: 'Generate Quiz', script, description })
 })
 
 router.get('/settings', auth.authenticated, async function (req, res, next) {
